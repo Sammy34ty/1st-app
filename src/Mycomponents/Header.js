65fd6e0084f7f8139f6b2e6205e342dfd0996378
Sammy <samattechdotcom@gmail.com>
2022-10-15 01:40:19 +0500

    error fixed

--- src/Mycomponents/Header.js
+++ src/Mycomponents/Header.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from "react-router-dom";
-function Header( props) {
+import { Link } from "react-router-dom";
+function Header(props) {
   return (
     <div>
-             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    {/* getting title from parent component via props.title */}
-    <Link className="navbar-brand" to="/">{props.title}</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home/</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/About"> Go To About</Link>
-        </li>
-        {/* <li className="nav-item dropdown">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          {/* getting title from parent component via props.title */}
+          <Link className="navbar-brand" to="/">{props.title}</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home/</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/About"> Go To About</Link>
+              </li>
+              {/* <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -30,24 +30,22 @@ function Header( props) {
             <li><a className="dropdown-item" to="#">Something else here</a></li>
           </ul>
         </li> */}
-        {/* <li className="nav-item">
+              {/* <li className="nav-item">
           <a className="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li> */}
-      </ul>
-        </form>:""}
-    </div>
-  </div>
-</nav>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
 export default Header
-Header.propTypes={
-    title: PropTypes.string,
-    searchbar: PropTypes.bool.isRequired
- }
- Header.defaultProps = {
-    title:"Your Titlr Here",
-    searchbar:true
- }   
- 
+Header.propTypes = {
+  title: PropTypes.string,
+  searchbar: PropTypes.bool.isRequired
+}
+Header.defaultProps = {
+  title: "Your Titlr Here",
+  searchbar: true
+}
